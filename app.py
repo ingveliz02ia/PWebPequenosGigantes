4be1d345f3c6829161b4e7694a20afd054f06cc7
ingveliz02ia <ingvelizia02@gmail.com>
2026-02-09 19:29:29 -0500

    L5

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
